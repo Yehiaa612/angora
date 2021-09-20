c5d3b96cfed25134e340d8f7f1a50c00e714edc7
Yehiaa612 <yehia_ashraf2000@hotmail.com>
2021-09-20 19:32:39 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// window.alert("hello")
+// document.getElementById("test").innerHTML="hello from js"
+// console.log("hello from js")
+var firstNumber =Number(prompt("please enter your first number"))
+var secondNumber =Number(prompt("please enter your seconde number"))
+console.log ("total is ="+(firstNumber+secondNumber));
